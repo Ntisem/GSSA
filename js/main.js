a551c8bdecd1f44bd0463506e700631dbd6fb61e
Ntisem <williamntisem123@gmail.comm>
2021-05-18 11:35:31 +0000

    pages

--- js/main.js
+++ js/main.js
@@ -248,6 +248,43 @@ $(window).on('load', function() {
 		}
 	
 	}
+		// media popup
+		function mediaPopup() {
+			$('.gallery-popup').colorbox({
+				rel: 'gallery-popup',
+				transition: 'slideshow',
+				innerHeight: '500'
+			});
+			$('.popup').colorbox({
+				iframe: true,
+				innerWidth: 600,
+				innerHeight: 400
+			});
+		}
+		mediaPopup();
+
+		// Shuffle js filter and masonry
+		function projectShuffle() {
+			if ($('.shuffle-wrapper').length !== 0) {
+				var Shuffle = window.Shuffle;
+				var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+					itemSelector: '.shuffle-item',
+					sizer: '.shuffle-sizer',
+					buffer: 1
+				});
+				$('input[name="shuffle-filter"]').on('change', function (evt) {
+					var input = evt.currentTarget;
+					if (input.checked) {
+						myShuffle.filter(input.value);
+					}
+				});
+				$('.shuffle-btn-group label').on('click', function () {
+					$('.shuffle-btn-group label').removeClass('active');
+					$(this).addClass('active');
+				});
+			}
+		}
+		projectShuffle();
 
 })(jQuery);
 
